2a7515fe964155d3515022c70af3033876d5b42b
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-04-06 12:18:29 +0200

    comment

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var landingPageButton = document.getElementById("landing-page-button");
 
 function openDoor(){
     console.log("openDoor() called");
+    //@TODO: logic to change one displayed image into another when this button is pressed 
 
 
 }
