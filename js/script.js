ba94dc23d6757f03c7a7f33c8d1ae5e4283dc49e
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-04-06 12:15:25 +0200

    implemented basic event handling

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var landingPageButton = document.getElementById("landing-page-button");
+
+function openDoor(){
+    console.log("openDoor() called");
+
+
+}
+
+landingPageButton.addEventListener("click", openDoor);
