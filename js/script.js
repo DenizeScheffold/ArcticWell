2dd0925b7a13d2f751dfdf4af0404694d7ef87e5
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-04-06 14:34:38 +0200

    implemented functional eventListener

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 var landingPageButton = document.getElementById("landing-page-button");
+var openDoorImg = document.getElementById("open-door");
+var closedDoorImg = document.getElementById("closed-door");
 
 function openDoor(){
     console.log("openDoor() called");
     //@TODO: logic to change one displayed image (door_close.png) into another (door_open.png) when this button is pressed 
+    closedDoorImg.style.display = "none";
+    openDoorImg.style.display = "inline";
+
 
 
 }
