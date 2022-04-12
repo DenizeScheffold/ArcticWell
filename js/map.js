23d7e785a875db3993060e95d25cf48d5276aa66
Denize <DenizeScheffold@gmail.com>
2022-04-12 12:00:18 +0200

    move script for map to map.js

--- js/map.js
+++ js/map.js
@@ -1,2 +1,33 @@
 
 
+
+//<!-- https://softauthor.com/show-user-location-on-google-maps-using-javascript/ -->
+
+navigator.geolocation.getCurrentPosition(
+   function (position) {
+      initMap(position.coords.latitude, position.coords.longitude)
+   },
+   function errorCallback(error) {
+      console.log(error)
+   }
+);
+
+function initMap(lat, lng) {
+var myLatLng = {
+   lat,
+   lng
+};
+ //  var marker = new google.maps.Marker({
+  //     position: myLatLng,
+  //     map: map,
+  //  });
+  
+
+
+var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: myLatLng,
+      mapTypeControl: false
+      
+  });
+}
