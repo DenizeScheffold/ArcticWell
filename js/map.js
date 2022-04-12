b49ce90d6b092d4ee33f4acc258c357be7506229
Denize <DenizeScheffold@gmail.com>
2022-04-12 13:28:22 +0200

    remove button for "get your position"

--- js/map.js
+++ js/map.js
@@ -1,47 +1,29 @@
-
-
-
 //<!-- https://softauthor.com/show-user-location-on-google-maps-using-javascript/ -->
 
 navigator.geolocation.getCurrentPosition(
-   function (position) {
-      initMap(position.coords.latitude, position.coords.longitude)
-   },
-   function errorCallback(error) {
-      console.log(error)
-   }
+  function (position) {
+    initMap(position.coords.latitude, position.coords.longitude);
+  },
+  function errorCallback(error) {
+    console.log(error);
+  }
 );
 
 function initMap(lat, lng) {
-var myLatLng = {
-   lat,
-   lng
-};
- //  var marker = new google.maps.Marker({
-  //     position: myLatLng,
-  //     map: map,
-  //  });
-  
+  var myLatLng = {
+    lat,
+    lng,
+  };
 
+   // var marker = new google.maps.Marker({
+   //    position: myLatLng,
+   //    map: map,
+   // });
 
-var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15,
-      center: myLatLng,
-      mapTypeControl: false
-      
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: myLatLng,
+    mapTypeControl: false,
   });
 
-  /*
-
-  <!-- 
-<script>
-  function myMap() {
-  var mapProp= {
-    center:new google.maps.LatLng(57.305998776,18.020166586),
-    zoom:15,
-  };
-  var map = new google.maps.Map(document.getElementById("map"),mapProp);
-  }
-  </script> --> 
-  */
 }
