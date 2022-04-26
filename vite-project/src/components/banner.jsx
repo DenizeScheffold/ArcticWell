dd88f67149104d575ffabadab9c8ddcb471b14dc
Denize <DenizeScheffold@gmail.com>
2022-04-26 13:08:55 +0200

    move components for map

--- vite-project/src/components/banner.jsx
+++ vite-project/src/components/banner.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
 
 export default function Banner() {
   return (
-    <div class="banner-container"></div>
-  )
+    <div class="banner-container">
+      <div class="arctic_well_logo"></div>
+    </div>
+  );
 }
-
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Banner from './components/banner'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Banner from "./components/banner.jsx";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Banner />
   </React.StrictMode>,
-  document.getElementById('root'),
-)
+  document.getElementById("root")
+);
