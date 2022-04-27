fb5d509a6b7aa07d00f565a7d040ae953d0fdd0a
Denize <DenizeScheffold@gmail.com>
2022-04-27 15:21:23 +0200

    return some of index code

--- next-project/arctic-well/.next/static/webpack/webpack.793862219888bd06.hot-update.js
+++ next-project/arctic-well/.next/static/webpack/webpack.793862219888bd06.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "f3369250fc2c0c62"; }
+/******/ 	__webpack_require__.h = function() { return "c9dd00a66ae33968"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
