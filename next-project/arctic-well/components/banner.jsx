c9f6c95fe97e92141a04ce5be424904bd58f4e8e
Denize <DenizeScheffold@gmail.com>
2022-04-27 14:44:18 +0200

    start for banner

--- next-project/arctic-well/components/banner.jsx
+++ next-project/arctic-well/components/banner.jsx
@@ -1,17 +1,26 @@
 import styles from "../styles/Home.module.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import arctic_well_logo from "./public/arctic_well_logo.svg";
 
 export default function Banner() {
   return (
     <div className="banner-container">
       <div className="line">
         <div className="BANNER">
-          <img src="Denize.jpeg" className="arctic_well_logo"/>
+          {/* <Image
+            alt="icon"
+            src={arctic_well_logo.svg}
+            placeholder="blur"
+            width={700}
+            height={475}
+            className="arctic_well_logo"
+          /> */}
+
           <div className="weather_subcontainer">
             <span className="Stockholm">Stockholm </span>
             <span className="-C">+31 °C</span>
-            <div className="Vector"></div>
+            <img src="./public/bi_sun.svg" className="Vector" />
           </div>
         </div>
       </div>
