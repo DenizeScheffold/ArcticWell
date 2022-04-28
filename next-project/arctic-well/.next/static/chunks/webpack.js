861efff851112af629a42837ab3c979e63c587c2
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-04-28 09:58:20 +0200

    functional implementation of localhost:3000/map

--- next-project/arctic-well/.next/static/chunks/webpack.js
+++ next-project/arctic-well/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eae36a66ff3dce77"; }
+/******/ 		__webpack_require__.h = function() { return "d757de6965166a54"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next-project/arctic-well/pages/map.js
+++ next-project/arctic-well/pages/map.js
@@ -1,24 +1,56 @@
+import {useEffect, useRef} from 'react';
+import {Loader} from '@googlemaps/js-api-loader';
+
 const Map = () => {
+  const googlemap = useRef(null);
+
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: 'AIzaSyAX7mdZbBYLkHDuDERyWCxBju2EpZGJ3Ac',
+      version: 'weekly'
+    });
+    let map;
+    loader.load().then(() => {
+      map = new google.maps.Map(googlemap.current, {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8,
+      });
+    });
+  });
+
   return (
-    <div class="container">
-      <div class="col-sm col-sm-12">
-        <p class="header-map">Find your Arctic Well</p>
-
-        <p id="location"></p>
-        <div class="flex-container">
-          <div class="frame">
-            <div id="map"></div>
-
-            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAX7mdZbBYLkHDuDERyWCxBju2EpZGJ3Ac&libraries=geometry&callback=initMap"></script>
-
-            <script src="./src/js/map.js"></script>
-
-            <div id="nearest"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div id="map" ref={googlemap} />
   );
-};
+}
 
 export default Map;
+
+
+// ***********************************
+// Old code that doesn't work anymore
+// ***********************************
+
+// const Map = () => {
+//   return (
+//     <div class="container">
+//       <div class="col-sm col-sm-12">
+//         <p class="header-map">Find your Arctic Well</p>
+
+//         <p id="location"></p>
+//         <div class="flex-container">
+//           <div class="frame">
+//             <div id="map"></div>
+
+//             <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAX7mdZbBYLkHDuDERyWCxBju2EpZGJ3Ac&libraries=geometry&callback=initMap"></script>
+
+//             <script src="./src/js/map.js"></script>
+
+//             <div id="nearest"></div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Map;
