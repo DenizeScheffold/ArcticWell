331884bf1cb41a38fa3fa49620c564bbe38d0033
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-05-03 13:10:41 +0200

    partial fixes to map, minor updates to gitignore

--- next-project/arctic-well/pages/map.jsx
+++ next-project/arctic-well/pages/map.jsx
@@ -1,39 +1,43 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
-
 
 // @TODO: make geolocation work
 // See https://developers.google.com/maps/documentation/javascript/react-map
 
 const Map = () => {
   const googlemap = useRef(null);
+  // dummy values that point to STI in Liljeholmen
+  const [pos, setPos] = useState({
+    lat: 59.3095651,
+    lng: 18.0194099,
+  });
 
-  // all calls to navigator.geolocation has to happen inside of useEffect
+  // Amusingly enough, this implementation "works" if this file is updated while on localhost:3000/map
+  // The issue is getting useEffect to automatically run again when pos is updated
+  // Attempts to use setPos or to change [pos] at the end of useEffect to [pos.lat] have so far not yielded any results
   useEffect(() => {
+    console.log("start of useEffect");
+    console.log("pos.lat=" + pos.lat + ", pos.lng=" + pos.lng);
+    navigator.geolocation.getCurrentPosition(function (position) {
+      pos.lat = position.coords.latitude;
+      pos.lng = position.coords.longitude;
+    }); //
+    console.log("after call to getCurrentPosition");
+    console.log("pos.lat=" + pos.lat + ", pos.lng=" + pos.lng);
     const loader = new Loader({
       apiKey: "AIzaSyAX7mdZbBYLkHDuDERyWCxBju2EpZGJ3Ac",
       version: "weekly",
     });
-    //for testing purposes only
-    if ("geolocation" in navigator) {
-      // console.log("geolocation available");
-      navigator.geolocation.getCurrentPosition(function (position) {
-        console.log("Latitude is :", position.coords.latitude);
-        console.log("Longitude is :", position.coords.longitude);
-      });
-    } else {
-      console.log("geolocation unavailable");
-    }
-    let map;
     loader.load().then(() => {
       const google = window.google;
+      let map;
       map = new google.maps.Map(googlemap.current, {
-        center: { lat: 59.3095651, lng: 18.0194099 },
+        center: pos,
         zoom: 15,
       });
     });
-  });
+  }, [pos]);
 
   return <div id="map" ref={googlemap} />;
 };
