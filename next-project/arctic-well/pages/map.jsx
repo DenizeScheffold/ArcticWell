3584446d2a282614a3ee67d971c40841942912e7
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-05-04 13:05:50 +0200

    slightly hacky (but functional) way of centering the map on the user

--- next-project/arctic-well/pages/map.jsx
+++ next-project/arctic-well/pages/map.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
-
+//
 const Map = () => {
   const googlemap = useRef(null);
   let map;
@@ -34,25 +33,36 @@ const Map = () => {
       console.log("geolocation available");
       navigator.geolocation.getCurrentPosition(function (position) {
         setPos({lat: position.coords.latitude, lng: position.coords.longitude})
-        console.log("inside getCurrentPosition, pos.lat=" + pos.lat + ", pos.lng=" + pos.lng);
       });
-      console.log("outside getCurrentPosition, pos.lat=" + pos.lat + ", pos.lng=" + pos.lng);
     }
-    // Currently only set to run once; in the future it should run continuously (possibly via watchCurrentPosition???)
-  },[]);
+    else{
+      console.log("geolocation unavailable");
+    }
+    // @TODO: Currently only set to run once; in the future it should maybe run continuously (possibly via watchCurrentPosition???)
+  }, []);
 
-  // Not functional (yet)
+  // Functional for now, but likely won't work properly if we want to add markers to the map at runtime
   useEffect(() => {
+    console.log("entered useEffect #3 -- pos.lat or pos.lng updated");
+    // Enter this block if pos.lat or pos.lng still have the initial value of 0
     if(!pos.lat || !pos.lng){
-      // console.log("!pos=" + !pos);
-      console.log("pos.lat or pos.lng updated");
+      // We don't need to do anything here for now
+      // console.log("inside if(!pos.lat || !pos.lng)");
+      // console.log("pos.lat=" + pos.lat + ", pos.lng=" + pos.lng);
+    }
+    else{
+      // This is where we re-center the map on the user's coordinates
+      console.log("inside else-block");
       console.log("!pos.lat=" + !pos.lat + ", !pos.lng=" + !pos.lng);
       console.log("pos.lat=" + pos.lat + ", pos.lng=" + pos.lng);
-      // map = google.maps.Map(googlemap.current, {
-      //   center: { lat: pos.lat, lng: pos.lng },
-      // });
-      // console.log("map re-centered");
-    }
+      // @TODO: this should update the existing map rather than create a new one
+      // it's proving to be very difficult to access the initialized one though
+      map = new google.maps.Map(googlemap.current, {
+        center: {lat: pos.lat, lng: pos.lng},
+        zoom: 15,
+      });
+      console.log("map re-centered");
+    } 
   }, [pos.lat, pos.lng]);
 
   return <div id="map" ref={googlemap} />;
