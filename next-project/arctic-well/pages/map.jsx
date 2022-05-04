087bff6889c341e4d5d40f3a6000fe0408714135
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-05-04 15:51:42 +0200

    a possibly better way of initializing the map. Hacky but working

--- next-project/arctic-well/pages/map.jsx
+++ next-project/arctic-well/pages/map.jsx
@@ -1,72 +1,112 @@
-import { useState, useEffect, useRef } from "react";
+import { memo, useState, useEffect, useRef, useCallback } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+// @TODO: center and containerStyle both suck and should be moved elsewhere
+const center = { 
+  lat: 59.3095651,
+  lng: 18.0194099,
+};
+
+const containerStyle = {
+  width: "100%",
+  height: "82%"
+};
 
 const Map = () => {
-  const googlemap = useRef(null);
-  let map;
-  // dummy values that point to STI in Liljeholmen
-  const [pos, setPos] = useState({
-    lat: 0,
-    lng: 0,
+  // const googlemap = useRef(null);
+  // // let map;
+  // // dummy values that point to STI in Liljeholmen
+  // const [pos, setPos] = useState({
+  //   lat: 0,
+  //   lng: 0,
+  // });
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyAX7mdZbBYLkHDuDERyWCxBju2EpZGJ3Ac"
   });
 
-  // Initialize the map with dummy coordinates
-  useEffect(() => {
-    const loader = new Loader({
-      apiKey: "AIzaSyAX7mdZbBYLkHDuDERyWCxBju2EpZGJ3Ac",
-      version: "weekly",
-    });
-    loader.load().then(() => {
-      const google = window.google;
-      map = new google.maps.Map(googlemap.current, {
-        center: { lat: 59.3095651, lng: 18.0194099 },
-        zoom: 15,
-      });
-      console.log("map initialized");
-    });
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback(function callback(map){
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map);
   }, []);
+
+  const onUnmount = useCallback(function callback(map){
+    setMap(null);
+  }, []);
+
+
+  // Initialize the map with dummy coordinates
+  // useEffect(() => {
+  //   const loader = new Loader({
+  //     apiKey: "AIzaSyAX7mdZbBYLkHDuDERyWCxBju2EpZGJ3Ac",
+  //     version: "weekly",
+  //   });
+  //   loader.load().then(() => {
+  //     const google = window.google;
+  //     map = new google.maps.Map(googlemap.current, {
+  //       center: { lat: 59.3095651, lng: 18.0194099 },
+  //       zoom: 15,
+  //     });
+  //     console.log("map initialized");
+  //   });
+  // }, []);
 
   // Geolocate the user via getCurrentPosition
   // Functional, but likely needs expanding to cover all our needs
-  useEffect(() => {
-    if("geolocation" in navigator){
-      console.log("geolocation available");
-      navigator.geolocation.getCurrentPosition(function (position) {
-        setPos({lat: position.coords.latitude, lng: position.coords.longitude})
-      });
-    }
-    else{
-      console.log("geolocation unavailable");
-    }
-    // @TODO: Currently only set to run once; in the future it should maybe run continuously (possibly via watchCurrentPosition???)
-  }, []);
+  // useEffect(() => {
+  //   if("geolocation" in navigator){
+  //     console.log("geolocation available");
+  //     navigator.geolocation.getCurrentPosition(function (position) {
+  //       setPos({lat: position.coords.latitude, lng: position.coords.longitude})
+  //     });
+  //   }
+  //   else{
+  //     console.log("geolocation unavailable");
+  //   }
+  //   // @TODO: Currently only set to run once; in the future it should maybe run continuously (possibly via watchCurrentPosition???)
+  // }, []);
 
   // Functional for now, but likely won't work properly if we want to add markers to the map at runtime
-  useEffect(() => {
-    console.log("entered useEffect #3 -- pos.lat or pos.lng updated");
-    // Enter this block if pos.lat or pos.lng still have the initial value of 0
-    if(!pos.lat || !pos.lng){
-      // We don't need to do anything here for now
-      // console.log("inside if(!pos.lat || !pos.lng)");
-      // console.log("pos.lat=" + pos.lat + ", pos.lng=" + pos.lng);
-    }
-    else{
-      // This is where we re-center the map on the user's coordinates
-      console.log("inside else-block");
-      console.log("!pos.lat=" + !pos.lat + ", !pos.lng=" + !pos.lng);
-      console.log("pos.lat=" + pos.lat + ", pos.lng=" + pos.lng);
-      // @TODO: this should update the existing map rather than create a new one
-      // it's proving to be very difficult to access the initialized one though
-      map = new google.maps.Map(googlemap.current, {
-        center: {lat: pos.lat, lng: pos.lng},
-        zoom: 15,
-      });
-      console.log("map re-centered");
-    } 
-  }, [pos.lat, pos.lng]);
+  // useEffect(() => {
+  //   console.log("entered useEffect #3 -- pos.lat or pos.lng updated");
+  //   // Enter this block if pos.lat or pos.lng still have the initial value of 0
+  //   if(!pos.lat || !pos.lng){
+  //     // We don't need to do anything here for now
+  //     // console.log("inside if(!pos.lat || !pos.lng)");
+  //     // console.log("pos.lat=" + pos.lat + ", pos.lng=" + pos.lng);
+  //   }
+  //   else{
+  //     // This is where we re-center the map on the user's coordinates
+  //     console.log("inside else-block");
+  //     console.log("!pos.lat=" + !pos.lat + ", !pos.lng=" + !pos.lng);
+  //     console.log("pos.lat=" + pos.lat + ", pos.lng=" + pos.lng);
+  //     // @TODO: this should update the existing map rather than create a new one
+  //     // it's proving to be very difficult to access the initialized one though
+  //     map = new google.maps.Map(googlemap.current, {
+  //       center: {lat: pos.lat, lng: pos.lng},
+  //       zoom: 15,
+  //     });
+  //     console.log("map re-centered");
+  //   } 
+  // }, [pos.lat, pos.lng]);
 
-  return <div id="map" ref={googlemap} />;
+  // return <div id="map" ref={googlemap} />;
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={15}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >{ /* Child components, such as markers, info windows, etc. */ }
+    <></></GoogleMap>
+  ) : <></>
 };
 
-export default Map;
+// export default Map;
+export default memo(Map);
