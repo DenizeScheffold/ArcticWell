d2c89f1281af8cad4683b579118abf0f6f20b842
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-05-05 17:59:17 +0200

    use standard formatting for the import

--- next-project/arctic-well/pages/_app.js
+++ next-project/arctic-well/pages/_app.js
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
-import variables from "../styles/variables.css";
+import "../styles/variables.css";
 
 function MyApp({ Component, pageProps }) {
   return (
