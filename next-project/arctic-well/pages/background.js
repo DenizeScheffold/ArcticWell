a5f845710964c49576a1ae7e5e989a4892c8c41f
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-05-10 14:12:50 +0200

    remove unused import

--- next-project/arctic-well/pages/background.js
+++ next-project/arctic-well/pages/background.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { bgWrap, bgText } from '../styles/styles.module.css'
+import { bgWrap } from '../styles/styles.module.css'
 
 // @TODO: might be better to apply the background through css instead of as a page. Refactor needed.
 const Background = () => (
