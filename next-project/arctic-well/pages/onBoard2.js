252fa7e2635f8333526bfaa9eef46afb9a14ebe3
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-05-12 13:40:47 +0200

    formatting

--- next-project/arctic-well/pages/onBoard2.js
+++ next-project/arctic-well/pages/onBoard2.js
@@ -3,52 +3,51 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-
 export default function onBoard() {
-    <Head>
-      <title>Welcome to Arctic Well</title>
-    </Head>;
-    return (
-
-      <div>
-
+  <Head>
+    <title>Welcome to Arctic Well</title>
+  </Head>;
+  return (
+    <div>
       <div className={styles.backgoundOnBord1}>
-      <Image
-        alt="boxPicture"
-        layout="responsive"
-        src="/map_hand.png"
-        width={377}
-        height={706}
-        quality={100}
-         />
-        </div>
-  
-      <div className={styles.button2}>
-      <Image className={styles.rectangel2}
-        alt="Rectangel"
-        layout="responsive"
-        src="/Rectangle_blue.png"
-        height={20}
-        width={140}
-        quality={100} 
-         />
-        <a className={styles.buttonText2} href="./onBoard3" >
-        Next
-        </a>
-        </div>
-        
-        <div className={styles.textMap}>
-           <h1>Map</h1>
-           </div>
-        
-        <div className={styles.textMap2}>
-        <p>On the mapview you can find all our boxes and your nearest Arctic Box</p>
-       </div>
-
-       <div className={styles.textskip}>
-        <a href="./map" >skip</a>
-       </div>
-    
+        <Image
+          alt="boxPicture"
+          layout="responsive"
+          src="/map_hand.png"
+          width={377}
+          height={706}
+          quality={100}
+        />
       </div>
-    )
-    }
+
+      <div className={styles.button2}>
+        <Image
+          className={styles.rectangel2}
+          alt="Rectangel"
+          layout="responsive"
+          src="/Rectangle_blue.png"
+          height={20}
+          width={140}
+          quality={100}
+        />
+        <a className={styles.buttonText2} href="./onBoard3">
+          Next
+        </a>
+      </div>
+
+      <div className={styles.textMap}>
+        <h1>Map</h1>
+      </div>
+
+      <div className={styles.textMap2}>
+        <p>
+          On the mapview you can find all our boxes and your nearest Arctic Box
+        </p>
+      </div>
+
+      <div className={styles.textskip}>
+        <a href="./map">skip</a>
+      </div>
+    </div>
+  );
+}
--- next-project/arctic-well/pages/onBoard3.js
+++ next-project/arctic-well/pages/onBoard3.js
@@ -3,52 +3,51 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-
 export default function onBoard() {
-    <Head>
-      <title>Welcome to Arctic Well</title>
-    </Head>;
-    return (
-
+  <Head>
+    <title>Welcome to Arctic Well</title>
+  </Head>;
+  return (
+    <div>
       <div>
-
-      <div >
-      <Image
-        alt="card"
-        layout="responsive"
-        src="/card.png"
-        width={377}
-        height={706}
-        quality={100}
-         />
-        </div>
-  
-      <div className={styles.button2}>
-      <Image className={styles.rectangel2}
-        alt="Rectangel"
-        layout="responsive"
-        src="/Rectangle_blue.png"
-        height={20}
-        width={140}
-        quality={100} 
-         />
-        <a className={styles.buttonText2} href="./onBoard4" >
-        Next
-        </a>
-        </div>
-        
-        <div className={styles.textCard}>
-           <h1>Easy Payment</h1>
-           </div>
-        
-        <div className={styles.textCard2}>
-        <p>Scan your card at the box to start your experience. Simple as that!</p>
-       </div>
-
-       <div className={styles.textskip}>
-        <a href="./map" >skip</a>
-       </div>
-    
+        <Image
+          alt="card"
+          layout="responsive"
+          src="/card.png"
+          width={377}
+          height={706}
+          quality={100}
+        />
       </div>
-    )
-    }
+
+      <div className={styles.button2}>
+        <Image
+          className={styles.rectangel2}
+          alt="Rectangel"
+          layout="responsive"
+          src="/Rectangle_blue.png"
+          height={20}
+          width={140}
+          quality={100}
+        />
+        <a className={styles.buttonText2} href="./onBoard4">
+          Next
+        </a>
+      </div>
+
+      <div className={styles.textCard}>
+        <h1>Easy Payment</h1>
+      </div>
+
+      <div className={styles.textCard2}>
+        <p>
+          Scan your card at the box to start your experience. Simple as that!
+        </p>
+      </div>
+
+      <div className={styles.textskip}>
+        <a href="./map">skip</a>
+      </div>
+    </div>
+  );
+}
