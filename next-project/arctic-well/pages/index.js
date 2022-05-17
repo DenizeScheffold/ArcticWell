dca330e15f9bc29cb64c9cb68683cdd027bc9563
Denize <DenizeScheffold@gmail.com>
2022-05-17 15:44:50 +0200

    try to fix

--- next-project/arctic-well/pages/index.js
+++ next-project/arctic-well/pages/index.js
@@ -9,36 +9,38 @@ export default function Home() {
     <title>Welcome to Arctic Well</title>
   </Head>;
   return (
-    <div>
+    <div className={styles.landing_container}>
       <div>
-        <Image
-          alt="articgang"
-          layout="responsive"
-          src="/background_landingpage.png"
-          width={377}
-          height={706}
-          quality={100}
-        />
-      </div>
-      <Link href="./onBoard1" className={styles.button} passHref>
-        <div className={styles.button}>
+        <div className={styles.bg_wrap}>
           <Image
-            alt="Rectangel"
-            layout="responsive"
-            src="/Rectangle.png"
-            height={39}
-            width={184}
+            alt="articgang"
+            layout="fixed"
+            src="/background_landingpage.png"
+            width={376}
+            height={736}
             quality={100}
+            priority
           />
           <a className={styles.buttonText}>Discover how it works!</a>{" "}
         </div>
-      </Link>
-      <div className={styles.text}>
-        <h1>Now it&apos;s easy to stay</h1>
       </div>
-
-      <div className={styles.textBreezy}>
-        <p>breezy!</p>
+      <div className={styles.flexbox_container}>
+        <Link href="./onBoard1" passHref>
+          <div className={styles.button}>
+            <Image
+              alt="Rectangle"
+              layout="fill"
+              src="/Rectangle_blue.png"
+              height={48}
+              width={272}
+              quality={100}
+            />
+            <a className={styles.buttonText}>Discover how it works!</a>{" "}
+          </div>
+        </Link>
+        <div className={styles.text}>
+          <h1>Now it&apos;s easy to stay breezy!</h1>
+        </div>
       </div>
     </div>
   );
--- next-project/arctic-well/pages/onBoard1.js
+++ next-project/arctic-well/pages/onBoard1.js
@@ -1,53 +1,54 @@
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Onboarding.module.css";
 
 export default function onBoard() {
   <Head>
     <title>Welcome to Arctic Well</title>
   </Head>;
   return (
-    <div>
+    <div className={styles.onboarding_container}>
       <div>
-        <Image
-          alt="boxPicture"
-          layout="responsive"
-          src="/our_boxes.png"
-          width={377}
-          height={706}
-          quality={100}
-        />
+        <div className={styles.bg_wrap}>
+          <Image
+            alt="boxPicture"
+            layout="fixed"
+            src="/onboarding_bg_1.png"
+            width={376}
+            height={736}
+            quality={100}
+            priority
+          />
+        </div>
       </div>
-
-      <div className={styles.button2}>
-        <Image
-          alt="Rectangel"
-          layout="responsive"
-          src="/Rectangle_blue.png"
-          height={20}
-          width={140}
-          quality={100}
-        />
-        <a className={styles.buttonText2} href="./onBoard2">
-          Next
-        </a>
-      </div>
-
-      <div className={styles.text2}>
-        <h1>Cooling, evolved</h1>
-      </div>
-
-      <div className={styles.text3}>
-        <p>
-          Say hello to the fastest and easiest way to reach a comfortable body
-          temperature. The experince will leave you feeling fresh and ready for
-          your next adventure.
-        </p>
-      </div>
-
-      <div className={styles.textskip}>
-        <a href="./map">skip</a>
+      <div className={styles.flexbox_container}>
+        <div className={styles.button}>
+          <Image
+            alt="Rectangle"
+            layout="fill"
+            src="/Rectangle_blue.png"
+            height={48}
+            width={272}
+            quality={100}
+          />
+          <a className={styles.buttonText} href="./onBoard2">
+            Next
+          </a>
+        </div>
+        <div className={styles.text1}>
+          <h1>Cooling, evolved</h1>
+        </div>
+        <div className={styles.text2}>
+          <p>
+            Say hello to the fastest and easiest way to reach a comfortable body
+            temperature. The experince will leave you feeling fresh and ready
+            for your next adventure.
+          </p>
+        </div>
+        <div className={styles.textskip}>
+          <a href="./map">skip</a>
+        </div>
       </div>
     </div>
   );
