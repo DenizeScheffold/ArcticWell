244f0a2508f110bf793b5cd75f58ad53974e5b65
Denize <DenizeScheffold@gmail.com>
2022-05-17 14:39:06 +0200

    Revert "add ayout to index.js"
    This reverts commit 19bbb2b0acb7b3cb7b7d219a6a776515e2bccd2f.

--- next-project/arctic-well/pages/index.js
+++ next-project/arctic-well/pages/index.js
@@ -7,40 +7,38 @@ import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <Layout>
-      <div className={styles.landing_container}>
-        <div>
-          <div className={styles.bg_wrap}>
-            <Image
-              alt="articgang"
-              layout="fixed"
-              src="/background_landingpage.png"
-              width={376}
-              height={736}
-              quality={100}
-              priority
-            />
-          </div>
-        </div>
-        <div className={styles.flexbox_container}>
-          <Link href="./onBoard1" passHref>
-            <div className={styles.button}>
-              <Image
-                alt="Rectangle"
-                layout="fill"
-                src="/Rectangle_blue.png"
-                height={48}
-                width={272}
-                quality={100}
-              />
-              <a className={styles.buttonText}>Discover how it works!</a>{" "}
-            </div>
-          </Link>
-          <div className={styles.text}>
-            <h1>Now it&apos;s easy to stay breezy!</h1>
-          </div>
+    <div className={styles.landing_container}>
+      <div>
+        <div className={styles.bg_wrap}>
+          <Image
+            alt="articgang"
+            layout="fixed"
+            src="/background_landingpage.png"
+            width={376}
+            height={736}
+            quality={100}
+            priority
+          />
         </div>
       </div>
-    </Layout>
+      <div className={styles.flexbox_container}>
+        <Link href="./onBoard1" passHref>
+          <div className={styles.button}>
+            <Image
+              alt="Rectangle"
+              layout="fill"
+              src="/Rectangle_blue.png"
+              height={48}
+              width={272}
+              quality={100}
+            />
+            <a className={styles.buttonText}>Discover how it works!</a>{" "}
+          </div>
+        </Link>
+        <div className={styles.text}>
+          <h1>Now it&apos;s easy to stay breezy!</h1>
+        </div>
+      </div>
+      </div>
   );
 }
