1b23566da21e4406515a2fdf7d3694bb59e36052
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-05-23 13:05:17 +0200

    fixes and tweaks to resolution on contact.js

--- next-project/arctic-well/pages/contact.js
+++ next-project/arctic-well/pages/contact.js
@@ -7,79 +7,82 @@ import Link from "next/link";
 
 export default function More() {
   return (
-    <><Head>
-      <title>Arctic Well</title>
-    </Head>
-   
-   <div>
-    <div className={styles.bg_wrap}>
-        <Image
-          alt="background"
-          layout="fixed"
-          src="/more_bg.jpg"
-          width={376}
-          height={736}
-          quality={100}
-          priority />
+    <div className={styles.more_container}>
+      <Head>
+        <title>Arctic Well</title>
+      </Head>
+
+      <div>
+        <div className={styles.bg_wrap}>
+          <Image
+            alt="background"
+            layout="fixed"
+            src="/more_bg.jpg"
+            width={376}
+            height={736}
+            quality={100}
+            priority
+          />
         </div>
-       
+
         <div className={styles.arrow}>
-        <Link href="./more" passHref>
-        <Image
-          alt="Arrow"
-          layout="fixed"
-          src="/Arrow.svg"
-          width={16}
-          height={15.93}
-          quality={100}
-          priority />
+          <Link href="./more" passHref>
+            <Image
+              alt="Arrow"
+              layout="fixed"
+              src="/Arrow.svg"
+              width={16}
+              height={16}
+              quality={100}
+            />
           </Link>
-          </div>
+        </div>
 
         <h1 className={styles.heading_text}>Contact us</h1>
- 
+
         <div className={styles.picture_health}>
-        <Image className={styles.picture_health}
-          alt="contact"
-          layout="fixed"
-          src="/contact.jpg"
-          width={344}
-          height={164}
-          quality={100}
-          priority />
+          <Image
+            className={styles.picture_health}
+            alt="contact"
+            layout="fixed"
+            src="/contact.jpg"
+            width={344}
+            height={164}
+            quality={100}
+          />
         </div>
 
         <div className={styles.icon}>
-        <Image 
-          alt="telephon"
-          layout="fixed"
-          src="/phone.svg"
-          width={56}
-          height={56}
-          quality={100}
-          priority />
+          <Image
+            alt="telephon"
+            layout="fixed"
+            src="/phone.svg"
+            width={56}
+            height={56}
+            quality={100}
+          />
         </div>
 
         <div className={styles.rent_nr}>
-        <p>+46 732 84 4566</p>
+          <p>08-555 55 55</p>
         </div>
 
         <div className={styles.icon}>
-        <Image 
-          alt="telephon"
-          layout="fixed"
-          src="/email.svg"
-          width={56}
-          height={56}
-          quality={100}
-          priority />
+          <Image
+            alt="telephon"
+            layout="fixed"
+            src="/email.svg"
+            width={56}
+            height={56}
+            quality={100}
+          />
         </div>
 
         <div className={styles.rent_email}>
-        <p>hello@arcticwell.com</p>
+          <p>hello@arcticwell.com</p>
         </div>
-
         <Navbar />
-      </div></>
+      </div>
+    </div>
   );
 }
