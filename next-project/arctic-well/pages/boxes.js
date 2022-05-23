44f7ba34a14ba67bfeb8700647232bdbc92f5d0e
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-05-23 12:52:19 +0200

    updated boxes.js to adjust to the correct resolution

--- next-project/arctic-well/pages/boxes.js
+++ next-project/arctic-well/pages/boxes.js
@@ -5,50 +5,54 @@ import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Link from "next/link";
 
-
+// @TODO: the text on this page needs a thorough do-over. The phrasing is a bit awkward
 export default function More() {
   return (
-    <><Head>
-      <title>Arctic Well</title>
-    </Head>
-   
-   <div>
-    <div className={styles.bg_wrap}>
-        <Image
-          alt="background"
-          layout="fixed"
-          src="/more_bg.jpg"
-          width={376}
-          height={736}
-          quality={100}
-          priority />
+    <div className={styles.more_container}>
+      <Head>
+        <title>Arctic Well</title>
+      </Head>
+
+      <div>
+        <div className={styles.bg_wrap}>
+          <Image
+            alt="background"
+            layout="fixed"
+            src="/more_bg.jpg"
+            width={376}
+            height={736}
+            quality={100}
+            priority
+          />
         </div>
-       
+
         <div className={styles.arrow}>
-        <Link href="./more" passHref>
-        <Image
-          alt="Arrow"
-          layout="fixed"
-          src="/Arrow.svg"
-          width={16}
-          height={15.93}
-          quality={100}
-          priority />
+          <Link href="./more" passHref>
+            <Image
+              alt="Arrow"
+              layout="fixed"
+              src="/Arrow.svg"
+              width={16}
+              height={16}
+              quality={100}
+            />
           </Link>
-          </div>
+        </div>
 
         <h1 className={styles.heading_text}>Our Boxes</h1>
-        <p className={styles.our_boxes_text}>We have a box for every occasion!</p>
- 
+        <p className={styles.our_boxes_text}>
+          We have a box for every occasion!
+        </p>
+
         <div className={styles.picture_box}>
-        <Image
-          alt="box"
-          layout="fixed"
-          src="/arctic_2.svg"
-          width={110}
-          height={211}
-          quality={100}
-          priority />
+          <Image
+            alt="box"
+            layout="fixed"
+            src="/arctic_2.svg"
+            width={110}
+            height={211}
+            quality={100}
+          />
         </div>
 
         <div className={styles.heading_text_2}>
@@ -56,49 +60,59 @@ export default function More() {
         </div>
 
         <div className={styles.city_text}>
-        <p>The standard City Box {<br></br>}sutable for 1 person or {<br></br>}possible 2.</p>
+          <p>
+            The standard City Box {<br></br>}suitable for 1 person or{" "}
+            {<br></br>}
+            possibly 2.
+          </p>
         </div>
 
         <div className={styles.list}>
-          <p>• -8 °C{<br></br>}• 1 Person{<br></br>}• 1x1 Meter</p>
+          <p>
+            • -8 °C{<br></br>}• 1 Person{<br></br>}• 1x1 Meters
+          </p>
         </div>
 
         <div className={styles.line}>
-        <Image
-          alt="line"
-          layout="fixed"
-          src="/Line.svg"
-          width={300}
-          height={815}
-          quality={100}
-          priority />
+          <Image
+            alt="line"
+            layout="fixed"
+            src="/Line.svg"
+            width={300}
+            height={815}
+            quality={100}
+          />
         </div>
 
         <div className={styles.heading_text_3}>
-          <h1>Arctic box Festival</h1>
+          <h1>Arctic Box Festival</h1>
         </div>
 
         <div className={styles.festival_box_text}>
-        <p>The Festival Box is for larger partys {<br></br>}and takes up to 10 persons.</p>
+          <p>
+            The Festival Box is for larger parties {<br></br>}and takes up to 10
+            persons.
+          </p>
         </div>
-       
+
         <div className={styles.list}>
-          <p>• -8 °C{<br></br>}• 1-10 Person{<br></br>}• 3x4 Meter</p>
+          <p>
+            • -8 °C{<br></br>}• 1-10 Person{<br></br>}• 3x4 Meters
+          </p>
         </div>
 
         <div className={styles.picture_bigbox}>
-        <Image
-          alt="bigbox"
-          layout="fixed"
-          src="/bigbooth.svg"
-          width={184}
-          height={162}
-          quality={100}
-          priority />
+          <Image
+            alt="bigbox"
+            layout="fixed"
+            src="/bigbooth.svg"
+            width={184}
+            height={162}
+            quality={100}
+          />
         </div>
-
-
         <Navbar />
-      </div></>
+      </div>
+    </div>
   );
 }
